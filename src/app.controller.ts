2568ebcec5adea08be735f45966ad3fcbf1408f4
fredrickUdom <fredrickudom@gmail.com>
2024-07-02 12:53:11 +0100

    fetching ip address

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,13 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Controller, Get, Query, Req } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Request } from 'express';
 
 @Controller('api')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get('hello')
-  getHello(@Query('visitor_name') visitorName: string,) {
-    return this.appService.getHello(visitorName);
+  getHello(@Query('visitor_name') visitorName: string, @Req() request: Request) {
+    return this.appService.getHello(visitorName, request);
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,9 +1,10 @@
-import { Injectable, Query } from '@nestjs/common';
+import { Injectable, Query, Req } from '@nestjs/common';
+import { Request } from 'express';
 
 @Injectable()
 export class AppService {
-  getHello(@Query("visitor_name") visitorName: string) {
-    const client_ip = '127.0.0.1';
+  getHello(@Query("visitor_name") visitorName: string, @Req() request: Request) {
+    const client_ip =  request.ip || '127.0.0.1';
     const location = 'New York';
     const temperature = 11;
     const greeting = `Hello, ${visitorName || 'Mark'}!, the temperature is ${temperature} degrees Celsius in ${location}`;
